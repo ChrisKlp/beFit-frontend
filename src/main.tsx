ed2fffbce690313db2bc53e3abb47ae819a6c3b9
CrisKLP <71100241+ChrisKlp@users.noreply.github.com>
2023-04-03 21:52:30 +0200

    Create initial routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
+import Layout from '@/components/Layout';
+import Home from '@/pages/Home';
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import Page404 from './pages/Page404';
 import theme from './theme';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />} errorElement={<Page404 />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
