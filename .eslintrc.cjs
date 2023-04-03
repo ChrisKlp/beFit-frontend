e129fc5bae4abae239e64bec87f822cb92bb74bc
CrisKLP <71100241+ChrisKlp@users.noreply.github.com>
2023-04-03 19:09:46 +0200

    Init

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': ['off'],
+    'react/require-default-props': ['off'],
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'react/button-has-type': ['warn'],
+    '@typescript-eslint/ban-types': ['warn'],
+    'import/extensions': ['off'],
+    'jsx-a11y/no-static-element-interactions': ['off'],
+  },
+  ignorePatterns: ['vite.config.ts', '*.cjs'],
+};
